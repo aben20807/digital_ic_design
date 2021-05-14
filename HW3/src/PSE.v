07f8519d0d03ea315f120cc96a289576a4d297cd
aben20807 <aben20807@gmail.com>
2021-05-14 12:48:55 +0800

    Finish hw3

--- HW3/src/PSE.v
+++ HW3/src/PSE.v
@@ -14,20 +14,19 @@ reg [9:0] Yout;
 
 reg [9:0] px [5:0];
 reg [9:0] py [5:0]; 
-reg signed [10:0] vx [5:0];
-reg signed [10:0] vy [5:0];
 wire cross_compare_result;
 
 reg [2:0] CurrState, Nextstate;
 parameter [2:0] InitState = 3'd0, 
                 InputState = 3'd1, 
-                Sort_even = 3'd2, 
+                Sort_odd_start = 3'd2,
                 Sort_odd = 3'd3,
-                OutputState = 3'd4,
-                Sort_even_start = 3'd5,
-                Sort_odd_start = 3'd6;
-                
-reg [2:0] in_i, out_i, sort_outer, i;
+                Sort_even_start = 3'd4,
+                Sort_even = 3'd5,
+                OutputState = 3'd6;
+
+reg [2:0] in_i, out_i, i, sort_outer;
+reg outer_enable; // to enable the outer_i counter, being high when inner loop finish
 integer j;
 
 // State
@@ -38,7 +37,9 @@ always @(posedge clk or posedge reset) begin
     CurrState <= Nextstate;
 end
 
-assign cross_compare_result = ((vx[i]*vy[i+1]) - (vx[i+1]*vy[i])) > 0;
+assign cross_compare_result = (
+  ($signed({1'b0,px[i  ]})-$signed({1'b0,px[0]}))*($signed({1'b0,py[i+1]})-$signed({1'b0,py[0]})) - 
+  ($signed({1'b0,px[i+1]})-$signed({1'b0,px[0]}))*($signed({1'b0,py[i  ]})-$signed({1'b0,py[0]}))) > 0;
 
 // Counter: in_i
 always @(posedge clk or posedge reset) begin
@@ -74,9 +75,13 @@ always @(posedge clk or posedge reset) begin
     sort_outer <= 3'd0;
   else begin
     case(CurrState)
-      Sort_odd:
-        sort_outer <= sort_outer + 3'd1;
-      Sort_even_start, Sort_even, Sort_odd_start:
+      Sort_odd, Sort_even: begin
+        if (outer_enable)
+          sort_outer <= (sort_outer == point_num)? sort_outer: sort_outer + 3'd1;
+        else
+          sort_outer <= sort_outer;
+      end
+      Sort_even_start, Sort_odd_start:
         sort_outer <= sort_outer;
       default:
         sort_outer <= 3'd0;
@@ -95,21 +100,19 @@ always @(posedge clk or posedge reset) begin
       Sort_odd_start:
         i <= 3'd1;
       Sort_even, Sort_odd:
-        i <= i + 3'd2;
+        i <= (i == point_num - 3'd1)? i : i + 3'd2;
       default:
        	i <= 3'd0;
     endcase
   end
 end
 
-// px py vx vy
+// px py
 always @(negedge clk or posedge reset) begin
   if (reset)
     for (j = 0; j < 6; j=j+1) begin
       px[j] <= 10'b0;
       py[j] <= 10'b0;
-      vx[j] <= 10'b0;
-      vy[j] <= 10'b0;
     end
   else begin
     case(CurrState)
@@ -118,8 +121,6 @@ always @(negedge clk or posedge reset) begin
       	 for (j = 0; j < 6; j=j+1) begin
           px[j] <= 10'b0;
           py[j] <= 10'b0;
-          vx[j] <= 10'b0;
-          vy[j] <= 10'b0;
         end
       end
 
@@ -127,16 +128,10 @@ always @(negedge clk or posedge reset) begin
       begin
         px[in_i] <= Xin;
         py[in_i] <= Yin;
-       	vx[in_i] <= Xin - px[0];
-        vy[in_i] <= Yin - py[0];
       end
       
       Sort_even, Sort_odd:
       begin
-        vx[i  ] <= (cross_compare_result) ? vx[i+1] : vx[i  ];
-        vx[i+1] <= (cross_compare_result) ? vx[i  ] : vx[i+1];
-        vy[i  ] <= (cross_compare_result) ? vy[i+1] : vy[i  ];
-        vy[i+1] <= (cross_compare_result) ? vy[i  ] : vy[i+1];
         px[i  ] <= (cross_compare_result) ? px[i+1] : px[i  ];
         px[i+1] <= (cross_compare_result) ? px[i  ] : px[i+1];
         py[i  ] <= (cross_compare_result) ? py[i+1] : py[i  ];
@@ -148,8 +143,6 @@ always @(negedge clk or posedge reset) begin
         for (j = 0; j < 6; j=j+1) begin
           px[j] <= px[j];
           py[j] <= py[j];
-       	  vx[j] <= vx[j];
-          vy[j] <= vy[j];
         end
       end
     endcase
@@ -158,6 +151,7 @@ end
 
 // Next Logic
 always @(point_num or CurrState or in_i or out_i or sort_outer or i) begin
+  outer_enable <= 0;
   case(CurrState)
     InitState:
       Nextstate <= InputState;
@@ -165,39 +159,43 @@ always @(point_num or CurrState or in_i or out_i or sort_outer or i) begin
     InputState:
     begin
       if (in_i == point_num - 3'd1)
-        Nextstate <= Sort_even_start;
+        Nextstate <= Sort_odd_start;
       else
         Nextstate <= InputState;
     end
-    
-    Sort_even_start:
-      Nextstate <= Sort_even;
-    
-    Sort_even:
-    begin
-      if (i >= point_num - 3'd3) begin
-        if (sort_outer == point_num - 3'd2)
-          Nextstate <= OutputState;
-        else
-          Nextstate <= Sort_odd_start;
-      end
-      else
-        Nextstate <= Sort_even;
-    end
-    
+
     Sort_odd_start:
       Nextstate <= Sort_odd;
 
     Sort_odd:
     begin
       if (i >= point_num - 3'd3) begin
-        if (sort_outer == point_num - 3'd2)
+        if (sort_outer >= point_num - 3'd1 || point_num == 3)
+          // early jump when point_num is 3 because there is only one comparison in need
           Nextstate <= OutputState;
-        else
+        else begin
+          outer_enable <= 1;
           Nextstate <= Sort_even_start;
+        end
       end
       else
         Nextstate <= Sort_odd;
+    end
+    Sort_even_start:
+      Nextstate <= Sort_even;
+
+    Sort_even:
+    begin
+      if (i >= point_num - 3'd3) begin
+        if (sort_outer >= point_num - 3'd1)
+          Nextstate <= OutputState;
+        else begin
+          outer_enable <= 1;
+          Nextstate <= Sort_odd_start;
+        end
+      end
+      else
+        Nextstate <= Sort_even;
     end
 
     OutputState:
